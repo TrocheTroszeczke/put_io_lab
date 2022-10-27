db6af2c06965c739f19b10708434d7fbe6a0ee1c
unknown <student@lab-44-1.cs.put.poznan.pl>
2022-10-27 16:00:25 +0200

    Changing id to student_id

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -1,14 +1,16 @@
 class Student
 { 
-    int id;//data member (also instance variable) 
+    int student_id;//data member (also instance variable) 
     String name; //data member (also instance variable) 
  
     public static void main(String args[])
     { 
         Student s1=new Student();//creating an object of Student 
-        System.out.println(s1.id); 
+        System.out.println(s1.student_id); 
         System.out.println(s1.name); 
+        System.out.println("Hello!");
      } 
 } 
 
 //This comment is a little change in the file
+//source: https://www.geeksforgeeks.org/classes-objects-java/
